2e8c7f063ae45a4f134a26348d4bbdc9d128f3b7
Lantana Tijani <144739521+LANTANA01@users.noreply.github.com>
2024-09-13 00:55:28 +0100

    Update 2-nested.js

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,4 +1,5 @@
 import { fromJS } from 'immutable';
+/*  edit it to return the value of the object at the defined path*/
 
 export default function accessImmutableObject(object, array) {
   const mappedObj = fromJS(object);

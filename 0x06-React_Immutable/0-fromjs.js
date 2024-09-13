65102b43d9e854b96fd8d688f6ba0631dc1e059e
Lantana Tijani <144739521+LANTANA01@users.noreply.github.com>
2024-09-13 01:01:06 +0100

    Update 0-fromjs.js

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,4 +1,7 @@
 import { fromJS } from 'immutable';
+/* create a function getImmutableObject that accepts
+object as a parameter and converts it into an immutable
+Map using fromJS of the Immutable.js library*/
 
 export default function getImmutableObject(object) {
   return fromJS(object);
